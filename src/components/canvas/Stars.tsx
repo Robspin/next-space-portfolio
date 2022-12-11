4fea28565e1d77f6ce420a7f84794dec8169e11b
Robin Steeman <robin@tracefy.com>
2022-12-11 09:39:25 +0100

    basic stars loop

---
+++ src/components/canvas/Stars.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useMemo, useRef } from "react"
+import { useFrame, useLoader } from "@react-three/fiber"
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import starPng from '../../../public/img/star.png'
+
+const Stars = ({ startingZ }: { startingZ: number }) => {
+    const count = 1000
+    const starSprite = useLoader(TextureLoader, starPng.src)
+    const starsRef = useRef<undefined | any>()
+
+    useFrame(() => {
+        starsRef.current.position.z += 0.08
+        starsRef.current.rotation.z += 0.0001
+
+        if (starsRef.current.position.z > 400) {
+            starsRef.current.position.z = -800
+        }
+    })
+
+    const positions = useMemo(() => {
+        const positions = []
+
+        for (let xi = 0; xi < count; xi++) {
+            const vertice = () => Math.random() * 600 - 300
+            positions.push(vertice(), vertice(), vertice())
+        }
+
+        return new Float32Array(positions)
+    }, [count])
+
+    useEffect(() => {
+        starsRef.current.position.z = startingZ
+    }, [])
+
+    return (
+        <points ref={starsRef}>
+            <bufferGeometry attach="geometry" >
+                <bufferAttribute
+                    attach={'attributes-position'}
+                    array={positions}
+                    count={positions.length / 3}
+                    itemSize={3}
+                />
+            </bufferGeometry>
+            <pointsMaterial attach="material" map={starSprite} color={0xaaaaaa} size={0.8} transparent={false}
+                            alphaTest={0.5} opacity={1.0} />
+        </points>
+    )
+}
+
+const StarsLoop = () => {
+
+    return (
+        <>
+            <Stars startingZ={0} />
+            <Stars startingZ={-400} />
+            <Stars startingZ={-800} />
+        </>
+    )
+}
+
+export default StarsLoop
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import { useEffect, useState } from "react"
 import dynamic from 'next/dynamic'
+import { Stars, Sky, PerspectiveCamera } from "@react-three/drei"
+
+import { PageTypes, NavigationClassTypes } from "@/types"
 import Landing from '@/components/layout/Landing'
 import AboutMe from "@/components/layout/AboutMe"
-import {useEffect, useState} from "react"
-import { PageTypes, NavigationClassTypes } from "@/types"
 import MyWork from "@/components/layout/MyWork"
 import Contact from "@/components/layout/Contact"
+import MovingStars from "@/components/canvas/Stars"
+import {Canvas, useThree} from "@react-three/fiber"
 
 // Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
@@ -52,7 +56,14 @@ export default function Page(props) {
 
 // Canvas components go here
 // It will receive same props as the Page component (from getStaticProps, etc.)
-Page.canvas = (props) => <Logo scale={0.5} route='/blob' position-y={-1} />
+Page.canvas = (props) => (
+    <>
+        <Logo scale={0.5} route='/blob' position-y={-1} />
+        {/*<Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={100000} />*/}
+        <Stars count={3000} />
+        <MovingStars />
+    </>
+)
 
 export async function getStaticProps() {
   return { props: { title: 'Index' } }
