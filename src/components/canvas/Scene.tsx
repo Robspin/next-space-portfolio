bcab879e1973653ce2f0fb3e5b86c60b74adef2c
Robin Steeman <robin@tracefy.com>
2022-12-11 10:06:48 +0100

    progress

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,5 +1,5 @@
 import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls, Preload } from '@react-three/drei'
+import {Preload, OrbitControlsProps, OrbitControls} from '@react-three/drei'
 
 export default function Scene({ children, ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
@@ -10,7 +10,7 @@ export default function Scene({ children, ...props }) {
       <ambientLight intensity={0.75} />
       {children}
       <Preload all />
-          {/*<OrbitControls />*/}
+      <OrbitControls enableZoom={false} />
     </Canvas>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react"
 import dynamic from 'next/dynamic'
-import { Stars, Sky, PerspectiveCamera } from "@react-three/drei"
 
 import { PageTypes, NavigationClassTypes } from "@/types"
 import Landing from '@/components/layout/Landing'
@@ -8,7 +7,6 @@ import AboutMe from "@/components/layout/AboutMe"
 import MyWork from "@/components/layout/MyWork"
 import Contact from "@/components/layout/Contact"
 import MovingStars from "@/components/canvas/Stars"
-import {Canvas, useThree} from "@react-three/fiber"
 
 // Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
@@ -60,7 +58,7 @@ Page.canvas = (props) => (
     <>
         <Logo scale={0.5} route='/blob' position-y={-1} />
         {/*<Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={100000} />*/}
-        <Stars count={3000} />
+        {/*<Stars count={3000} />*/}
         <MovingStars />
     </>
 )
